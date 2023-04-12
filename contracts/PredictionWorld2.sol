8a9ca1d409322e07c6e1f93d3a9e48b009576cf2
john.huang <john.huang@onelab.tw>
2023-04-12 11:58:35 +0800

    copy copy

--- contracts/PredictionWorld2.sol
+++ contracts/PredictionWorld2.sol
@@ -56,9 +56,54 @@ contract PredictionWorld2 {
     function createMarket(
         string memory _market,
         string memory _description,
-        string memory _resolveUrl,
+        string memory _resolverUrl,
         uint256 _endTimestamp
     ) public {
+        require(msg.sender == owner, "Unauthorized");
+        uint256 timestamp = block.timestamp;
+
+        Market storage market = markets[totalMarkets];
+        market.id = totalMarkets++;
+        market.market = _market;
+        market.timestamp = timestamp;
+        market.createdBy = msg.sender;
+        market.totalAmount = 0;
+        market.totalYesAmount = 0;
+        market.totalNoAmount = 0;
+        market.description = _description;
+        market.resolverUrl = _resolverUrl;
+        market.endTimestamp = _endTimestamp;
+
+        emit MarketCreated(
+            totalMarkets,
+            _market,
+            timestamp,
+            msg.sender,
+            0,
+            0,
+            0
+        );
+    }
+
+    function addYesBet(uint256 _marketId, uint256 _value) public payable {
+        require(_value <= ERC20(sureToken2).allowance(msg.sender, address(this)), "Not allowed to spend this ammount.");
+        Market storage market = markets[_marketId];
+        ERC20(sureToken2).transferFrom(msg.sender, address(this), _value);
+        AmountAdded memory amountAdded = AmountAdded(
+            msg.sender,
+            _value,
+            block.timestamp
+        );
+
+        market.totalYesAmount += _value;
+        market.totalAmount += _value;
+        market.yesCount.push(amountAdded);
+    }
+
+    function addNoBet(uint256 _marketId, uint256 _value) public payable {
+        require(_value <= ERC20(sureToken2).allowance(msg.sender, address(this)), "Not allowed to spend this amount.");
+        Market storage market = markets[_marketId];
+        ERC20(sureToken2).transferFrom(msg.sender, address(this), _value);
 
     }
-}
+ }
