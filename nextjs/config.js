0ca846fffec4fbe6f20c78216ef7bbbe3b562ca5
john.huang <john.huang@onelab.tw>
2023-04-13 19:38:47 +0800

    update scaffold

---
+++ nextjs/config.js
@@ -0,0 +1,5 @@
+//(mumbai)SURE Token(2) contract deployed to 0xd63E83580bBEc586665ABBD4e1B02f8bBD6F8bE0
+//(mumbai)PredictionWorld(2) contract deployed to 0xa33984BDD910d25C9d4ad11dD50E2ba18E0Ab7FD
+
+export const sureToken2Address = "0xd63E83580bBEc586665ABBD4e1B02f8bBD6F8bE0";
+export const predictionWorld2Address = "0xa33984BDD910d25C9d4ad11dD50E2ba18E0Ab7FD";
