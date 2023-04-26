60d890141c8168bff0ad03cfcfdddcd8ddc74d2f
Phil Kau <elsevierphil@gmail.com>
2023-04-26 08:28:33 +0800

    chore: update indent to 2 spaces

--- nextjs/pages/portfolio.js
+++ nextjs/pages/portfolio.js
@@ -11,119 +11,119 @@ import PortfolioMarketCard from "@/components/PortfolioMarketCard";
 
 
 export default function Portfolio() {
-    const [portfolioValue, setPortfolioValue] = useState(0);
-    const [personalBetInfo, setPersonalBetInfo] = useState([]);
-    
+  const [portfolioValue, setPortfolioValue] = useState(0);
+  const [personalBetInfo, setPersonalBetInfo] = useState([]);
 
-    const getMarkets = async () => {
-        try {
-            const { ethereum } = window;
-            const provider = new ethers.providers.Web3Provider(ethereum);
-            const signer = provider.getSigner();
-            const predictionWorldContract = new ethers.Contract(
-                predictionWorld3Address,
-                PredictionWorld.abi,
-                signer
-            );
 
-            const accounts = await ethereum.request({ method: "eth_accounts" });
-            const account = accounts[0];
+  const getMarkets = async () => {
+    try {
+      const { ethereum } = window;
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const predictionWorldContract = new ethers.Contract(
+        predictionWorld3Address,
+        PredictionWorld.abi,
+        signer
+      );
 
-            let marketCount = await predictionWorldContract.totalMarkets();
-            let markets = [];
-            for (let i = 0; i < marketCount; i++) {
-                let market = await predictionWorldContract.markets(i);
-                markets.push({
-                    id: market.id,
-                    title: market.question,
-                    imageHash: "", // temp holder
-                    totalAmount: market.totalAmount,
-                    totalYesAmount: market.totalYesAmount,
-                    totalNoAmount: market.totalNoAmount,
-                    hasResolved: market.marketClosed,
-                    endTimestamp: market.endTimestamp,
-                    timestamp: market.timestamp
-                });
-            }
-            console.log(`markets size: ${markets.length}`);
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      const account = accounts[0];
 
-            let personalizedBetInfo = [];
-            let totalBetAmount = 0;
-            for (let i = 0; i < markets.length; i++) {
-                let marketBets = await predictionWorldContract.getBets(i);
-                marketBets["0"].forEach((bet) => {
-                    if (bet[0].toLowerCase() == account.toLowerCase()) {
-                        personalizedBetInfo.push({
-                            id: i.toString(),
-                            yesAmount: bet[1].toString(),
-                            timestamp: bet[2].toString(),
-                        });
-                        totalBetAmount += parseInt(bet[1]);
-                    }
-                });
-                marketBets["1"].forEach((bet) => {
-                    if (bet[0].toLowerCase() == account.toLowerCase()) {
-                        personalizedBetInfo.push({
-                            id: i.toString(),
-                            noAmount: bet[1].toString(),
-                            timestamp: bet[2].toString(),
-                        });
-                        totalBetAmount += parseInt(bet[1]);
-                    }
-                });
-            }
-            setPortfolioValue(totalBetAmount);
-            for (let i = 0; i < personalizedBetInfo.length; i++) {
-                let market = markets.find((market) => market.id == personalizedBetInfo[i].id);
-                personalizedBetInfo[i].title = market?.title;
-                personalizedBetInfo[i].imageHash = market?.imageHash;
-                personalizedBetInfo[i].totalAmount = market?.totalAmount;
-                personalizedBetInfo[i].totalYesAmount = market?.totalYesAmount;
-                personalizedBetInfo[i].totalNoAmount = market?.totalNoAmount;
-                personalizedBetInfo[i].marketClosed = market?.marketClosed;
-                personalizedBetInfo[i].endTimestamp = market?.endTimestamp;
-                personalizedBetInfo[i].timestamp = market?.timestamp;
-            }
-            setPersonalBetInfo(personalizedBetInfo);
-        } catch (error) {
-            console.log(`Error getting markets, ${error}`);
-        }
+      let marketCount = await predictionWorldContract.totalMarkets();
+      let markets = [];
+      for (let i = 0; i < marketCount; i++) {
+        let market = await predictionWorldContract.markets(i);
+        markets.push({
+          id: market.id,
+          title: market.question,
+          imageHash: "", // temp holder
+          totalAmount: market.totalAmount,
+          totalYesAmount: market.totalYesAmount,
+          totalNoAmount: market.totalNoAmount,
+          hasResolved: market.marketClosed,
+          endTimestamp: market.endTimestamp,
+          timestamp: market.timestamp
+        });
+      }
+      console.log(`markets size: ${markets.length}`);
+
+      let personalizedBetInfo = [];
+      let totalBetAmount = 0;
+      for (let i = 0; i < markets.length; i++) {
+        let marketBets = await predictionWorldContract.getBets(i);
+        marketBets["0"].forEach((bet) => {
+          if (bet[0].toLowerCase() == account.toLowerCase()) {
+            personalizedBetInfo.push({
+              id: i.toString(),
+              yesAmount: bet[1].toString(),
+              timestamp: bet[2].toString(),
+            });
+            totalBetAmount += parseInt(bet[1]);
+          }
+        });
+        marketBets["1"].forEach((bet) => {
+          if (bet[0].toLowerCase() == account.toLowerCase()) {
+            personalizedBetInfo.push({
+              id: i.toString(),
+              noAmount: bet[1].toString(),
+              timestamp: bet[2].toString(),
+            });
+            totalBetAmount += parseInt(bet[1]);
+          }
+        });
+      }
+      setPortfolioValue(totalBetAmount);
+      for (let i = 0; i < personalizedBetInfo.length; i++) {
+        let market = markets.find((market) => market.id == personalizedBetInfo[i].id);
+        personalizedBetInfo[i].title = market?.title;
+        personalizedBetInfo[i].imageHash = market?.imageHash;
+        personalizedBetInfo[i].totalAmount = market?.totalAmount;
+        personalizedBetInfo[i].totalYesAmount = market?.totalYesAmount;
+        personalizedBetInfo[i].totalNoAmount = market?.totalNoAmount;
+        personalizedBetInfo[i].marketClosed = market?.marketClosed;
+        personalizedBetInfo[i].endTimestamp = market?.endTimestamp;
+        personalizedBetInfo[i].timestamp = market?.timestamp;
+      }
+      setPersonalBetInfo(personalizedBetInfo);
+    } catch (error) {
+      console.log(`Error getting markets, ${error}`);
     }
+  }
 
-    useEffect(() => {
-        getMarkets();
-    }, []);
+  useEffect(() => {
+    getMarkets();
+  }, []);
 
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Prediction World</title>
-                <meta name="description" content="Generated by create next app" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Navbar />
-            <main className="w-full flex flex-col sm:flex-row flex-wrap sm:flex-nowrap py-4 flex-grow max-w-5xl">
-                <div className="w-full flex flex-col pt-1">
-                    <div className="p-10 bg-blue-700 rounded-lg flex flex-row justify-evenly">
-                        <div className="flex flex-col items-center">
-                            <h1 className="text-white opacity-50 text-lg">Portfolio Value</h1>
-                            <h1 className="text-white text-4xl font-bold">
-                                {portfolioValue}{ " SURE" }
-                            </h1>
-                        </div>
-                    </div>
-                    <span className="font-bold my-3 text-lg">Your Market Positions</span>
-                    {personalBetInfo.map((market) => (
-                        <PortfolioMarketCard 
-                            title={market.title}
-                            totalYesAmount={market.totalYesAmount}
-                            totalNoAmount={market.totalNoAmount}
-                            endTimestamp={market.endTimestamp}
-                            timestamp={market.timestamp}                     
-                        />
-                    ))}
-                </div>
-            </main>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Prediction World</title>
+        <meta name="description" content="Generated by create next app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <main className="w-full flex flex-col sm:flex-row flex-wrap sm:flex-nowrap py-4 flex-grow max-w-5xl">
+        <div className="w-full flex flex-col pt-1">
+          <div className="p-10 bg-blue-700 rounded-lg flex flex-row justify-evenly">
+            <div className="flex flex-col items-center">
+              <h1 className="text-white opacity-50 text-lg">Portfolio Value</h1>
+              <h1 className="text-white text-4xl font-bold">
+                {portfolioValue}{" SURE"}
+              </h1>
+            </div>
+          </div>
+          <span className="font-bold my-3 text-lg">Your Market Positions</span>
+          {personalBetInfo.map((market) => (
+            <PortfolioMarketCard
+              title={market.title}
+              totalYesAmount={market.totalYesAmount}
+              totalNoAmount={market.totalNoAmount}
+              endTimestamp={market.endTimestamp}
+              timestamp={market.timestamp}
+            />
+          ))}
         </div>
-    );
+      </main>
+    </div>
+  );
 }
