c8690cd175078cc2b44d291ba894d75d87fe21c1
john.huang <john.huang@onelab.tw>
2023-05-02 14:25:05 +0800

    add charts

--- nextjs/components/ChartContainer.js
+++ nextjs/components/ChartContainer.js
@@ -1,4 +1,6 @@
 import { ethers } from "ethers";
+import { useEffect } from "react";
+import Plotly from 'plotly.js-dist-min';
 
 import { predictionWorld3Address } from "@/config";
 import PredictionWorld from "../utils/abis/PredictionWorld3.json";
@@ -14,14 +16,62 @@ export default function ChartContainer({ questionId }) {
                 PredictionWorld.abi,
                 signer
             );
+            //console.log(typeof questionId);
+            let bets = await predictionWorldContract.getBets(Number(questionId));
+            //console.log("bets");
+            //console.log(bets);
+            let yesBets = {
+                time: [],
+                amount: [],
+            };
+            let noBets = {
+                time: [],
+                amount: [],
+            };
+            // yes bets
+            bets["0"].forEach((bet) => {
+                yesBets.time.push(new Date(parseInt(bet.timestamp + "000")));
+                let currentSum = yesBets.amount.reduce((a, b) => a + b, 0);
+                yesBets.amount.push(currentSum + bet.amount.toNumber());
+            });
+            // no bets
+            bets["1"].forEach((bet) => {
+                noBets.time.push(new Date(parseInt(bet.timestamp + "000")));
+                let currentSum = noBets.amount.reduce((a, b) => a + b, 0);
+                noBets.amount.push(currentSum + bet.amount.toNumber());
+            });
+
+            const yesGraph = {
+                x: [...yesBets.time],
+                y: [...yesBets.amount],
+                mode: "lines+markers",
+                name: "Yes",
+            };
+
+            const noGraph = {
+                x: [...noBets.time],
+                y: [...noBets.amount],
+                mode: "lines+markers",
+                name: "No",
+            };
+
+            const chartData = [yesGraph, noGraph];
+            const layout = {
+                title: "Yes / No Graph",
+            };
+
+            Plotly.newPlot("myDiv", chartData, layout, { displayModeBar: false });
         } catch(error) {
             console.log(`Error getting bets, ${error}`);
         }
     }
 
+    useEffect(() => {
+        getBets();
+    });
+
     return (
         <>
-            {questionId}
             <div id="myDiv"></div>
         </>
     );
