0e8a1995530d5de3011c7a15958cd406ba818809
john.huang <john.huang@onelab.tw>
2023-05-04 12:56:49 +0800

    update config to new contracts

--- nextjs/config.js
+++ nextjs/config.js
@@ -8,13 +8,11 @@
 // PredictionWorld(3) contract deployed to 0xF9aE207F64C348aC22262C4F5E43C6b2538De22F
 // SURE Token(3) contract deployed to 0xaA47262af6d8028491996ad368d8d16Cc04439b3
 // PredictionWorld(3) contract deployed to 0x852Be5AD1aa3ce0d7a0a35De1f283e67E66761E3
-
-// deployed but abi not copied yet
 //SURE Token(3) contract deployed to 0x8744A70D3A8D5cC95Ef882cED9e62De94B0F3B75
 //PredictionWorld(3) contract deployed to 0xf3466268C35615c45704784bb6c58E2F72f53489
 
-export const sureToken3Address = "0xaA47262af6d8028491996ad368d8d16Cc04439b3";
-export const predictionWorld3Address = "0x852Be5AD1aa3ce0d7a0a35De1f283e67E66761E3";
+export const sureToken3Address = "0x8744A70D3A8D5cC95Ef882cED9e62De94B0F3B75";
+export const predictionWorld3Address = "0xf3466268C35615c45704784bb6c58E2F72f53489";
 
 export const sureToken2Address = "0xd63E83580bBEc586665ABBD4e1B02f8bBD6F8bE0";
 export const predictionWorld2Address = "0xa33984BDD910d25C9d4ad11dD50E2ba18E0Ab7FD";
