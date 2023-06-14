74565aa6ff144f9a0b4d772374496351ea8a07fc
Phil Kau <elsevierphil@gmail.com>
2023-06-14 20:55:34 +0800

    feat: update the scripts for upgrading

--- hardhat/scripts/upgradeProxy.js
+++ hardhat/scripts/upgradeProxy.js
@@ -4,18 +4,16 @@ const { PROXY_ADDRESS, UPGRADING_CONTRACT } = process.env;
 async function main() {
   const PredictionWorld = await ethers.getContractFactory(UPGRADING_CONTRACT);
   const upgraded = await upgrades.upgradeProxy(PROXY_ADDRESS, PredictionWorld);
-  
-  // The addres always get the elder one.
-  // You have to get the address from the PolygonScan by the Proxy address.
-  // const implementationAddress = await upgrades.erc1967.getImplementationAddress(
-  //   PROXY_ADDRESS
-  // );
+  await upgraded.deployed();
 
-  console.log('The current contract owner is: ' + await upgraded.owner());
-  console.log('Implementation contract: ' + UPGRADING_CONTRACT);
-  // console.log('Implementation contract address: ' + implementationAddress);
+  const implementationAddress = await upgrades.erc1967.getImplementationAddress(
+    PROXY_ADDRESS
+  );
+
+  console.log('                  Owner: ' + await upgraded.owner());
+  console.log('          Contract Name: ' + UPGRADING_CONTRACT);
+  console.log('         Proxy Contract: ' + PROXY_ADDRESS);
+  console.log('Implementation Contract: ' + implementationAddress);
 }
 
 main();
-
-// Implementation contract address: 0x75Ad513E3177A6e96E4FCE22fa4209617BC22e9B
