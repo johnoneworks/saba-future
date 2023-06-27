a44a1272b665413bc988120ecc372bb671e52907
Lydia Huang <loveyleslie1013@gmail.com>
2023-06-27 14:24:28 +0800

    feat: Market detail chart 在 desktop 時預設打開

--- nextjs/components/ChartContainer/ChartContainer.js
+++ nextjs/components/ChartContainer/ChartContainer.js
@@ -2,6 +2,8 @@ import { BiconomyAccountContext } from "@/contexts/BiconomyAccountContext";
 import { PageContext } from "@/contexts/PageContext";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { Box, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import classnames from "classnames";
 import { useCallback, useContext, useEffect, useState } from "react";
 import styles from "./ChartContainer.module.scss";
@@ -48,39 +50,47 @@ export default function ChartContainer() {
     }, [currentMarketID, account, getBets]);
 
     function InfoTable({ info, title, buttonStyle }) {
-        const [isOpen, setIsOpen] = useState(false);
+        const theme = createTheme();
+        const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
+        const [isOpen, setIsOpen] = useState(isDesktop);
+
+        useEffect(() => {
+            setIsOpen(isDesktop);
+        }, [isDesktop]);
 
         const handleClick = () => {
             setIsOpen(!isOpen);
         };
 
         return (
-            <Box sx={{ marginBottom: "20px" }}>
-                <Button className={[buttonStyle, classnames({ [styles.isOpen]: isOpen })].join(" ")} onClick={handleClick}>
-                    {title}
-                    <ExpandMoreIcon />
-                </Button>
-                {isOpen && (
-                    <TableContainer className={styles.tableContainer}>
-                        <Table>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell sx={{ width: "50%", textAlign: "center", fontSize: "12px", p: 0.5 }}>Time</TableCell>
-                                    <TableCell sx={{ width: "50%", textAlign: "center", fontSize: "12px", p: 0.5 }}>Amount</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {info.map((bet, i) => (
-                                    <TableRow key={i}>
-                                        <TableCell sx={{ width: "50%", textAlign: "center", borderBottom: "none" }}>{bet.time.toLocaleString()}</TableCell>
-                                        <TableCell sx={{ width: "50%", textAlign: "center", borderBottom: "none" }}>{bet.amount}</TableCell>
+            <ThemeProvider theme={theme}>
+                <Box sx={{ marginBottom: "20px" }}>
+                    <Button className={[buttonStyle, classnames({ [styles.isOpen]: isOpen })].join(" ")} onClick={handleClick}>
+                        {title}
+                        <ExpandMoreIcon className={styles.arrowIcon} />
+                    </Button>
+                    {isOpen && (
+                        <TableContainer className={styles.tableContainer}>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell sx={{ width: "50%", textAlign: "center", fontSize: "12px", p: 0.5 }}>Time</TableCell>
+                                        <TableCell sx={{ width: "50%", textAlign: "center", fontSize: "12px", p: 0.5 }}>Amount</TableCell>
                                     </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                )}
-            </Box>
+                                </TableHead>
+                                <TableBody>
+                                    {info.map((bet, i) => (
+                                        <TableRow key={i}>
+                                            <TableCell sx={{ width: "50%", textAlign: "center", borderBottom: "none" }}>{bet.time.toLocaleString()}</TableCell>
+                                            <TableCell sx={{ width: "50%", textAlign: "center", borderBottom: "none" }}>{bet.amount}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    )}
+                </Box>
+            </ThemeProvider>
         );
     }
 
