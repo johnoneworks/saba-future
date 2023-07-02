8b3a9d71fea6cb593057305c2a5c1f66b06fdfd4
Yufeng Kao <yufengkao@gorilla-technology.com>
2023-07-03 00:19:51 +0800

    feat: migrate from hardhat-waffle to hardhat-toolbox

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,5 +1,4 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
+require("@nomicfoundation/hardhat-toolbox");
 require('@openzeppelin/hardhat-upgrades');
 require("dotenv").config();
 
