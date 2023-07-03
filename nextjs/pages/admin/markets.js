35bf94026b95de16895e4b92c372a755bc9f919b
jim huang <jim.huang@onelab.tw>
2023-07-03 16:17:05 +0800

    feat: update admin markets style

--- nextjs/pages/admin/markets.js
+++ nextjs/pages/admin/markets.js
@@ -1,10 +1,37 @@
 import AdminMarketCard from "@/components/AdminMarketCard";
 import { AdminHeader } from "@/components/Header/AdminHeader";
-import { BACKUP_IMAGE } from "@/constants/Constant";
+import { BACKUP_IMAGE, MARKET_STATUS } from "@/constants/Constant";
 import { BiconomyAccountContext } from "@/contexts/BiconomyAccountContext";
-import { Button } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import Link from "next/link";
 import { useContext, useEffect, useState } from "react";
+
+const ShowMarkets = (props) => {
+    const { marketStatus, markets } = props;
+    const isClose = marketStatus !== MARKET_STATUS.OPEN;
+
+    return (
+        <>
+            {isClose && (
+                <Typography variant="h6" sx={{ width: "100%", textAlign: "center", my: 1, mt: 2 }}>
+                    Closed Markets
+                </Typography>
+            )}
+            {markets &&
+                markets.reduce((accumulator, market) => {
+                    const isAvailable = isClose ? market.hasResolved : !market.hasResolved;
+                    if (isAvailable) {
+                        accumulator.push(
+                            <div key={market.id} className="w-1/2 pr-2">
+                                <AdminMarketCard id={market.id} market={market} />
+                            </div>
+                        );
+                    }
+                    return accumulator;
+                }, [])}
+        </>
+    );
+};
 
 export default function Markets() {
     const [markets, setMarkets] = useState([]);
@@ -48,14 +75,12 @@ export default function Markets() {
                         Back
                     </Button>
                 </Link>
-
-                <main className="w-full flex flex-row flex-wrap py-4 max-w-5xl pb-6">
-                    {markets.map((market) => (
-                        <div key={market.id} className="w-1/2 pr-2">
-                            <AdminMarketCard id={market.id} market={market} />
-                        </div>
-                    ))}
-                </main>
+                <Grid spacing={2} columns={{ xs: 12, sm: 12, md: 12 }}>
+                    <main className="w-full flex flex-row flex-wrap py-4 max-w-5xl pb-6">
+                        <ShowMarkets marketStatus={MARKET_STATUS.OPEN} markets={markets} />
+                        <ShowMarkets marketStatus={MARKET_STATUS.CLOSE} markets={markets} />
+                    </main>
+                </Grid>
             </div>
         </>
     );
