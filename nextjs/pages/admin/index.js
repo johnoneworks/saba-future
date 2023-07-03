f083841897a21a9579835431abe4458712e2b288
jim huang <jim.huang@onelab.tw>
2023-07-03 16:58:05 +0800

    feat: fix create market image error & loading

--- nextjs/pages/admin/index.js
+++ nextjs/pages/admin/index.js
@@ -1,16 +1,19 @@
 import { AdminHeader } from "@/components/Header/AdminHeader";
 import { predictionWorldAddress } from "@/config";
 import { BiconomyAccountContext } from "@/contexts/BiconomyAccountContext";
+import { LoadingContext } from "@/contexts/LoadingContext";
 import { Box, Button, Container, TextField, Typography } from "@mui/material";
 import { ethers } from "ethers";
 import Link from "next/link";
 import { useCallback, useContext, useEffect, useState } from "react";
 
 export default function Admin() {
+    const { isPageLoading } = useContext(LoadingContext);
     const [submitButtonText, setSubmitButtonText] = useState("Create Market");
     const [balance, setBalance] = useState(0);
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
     const [resolverUrl, setResolverUrl] = useState("");
     const [timestamp, setTimestamp] = useState(Date());
     const { provider, sureTokenContract, account, smartAccount, predictionWorldInterface } = useContext(BiconomyAccountContext);
@@ -43,7 +46,7 @@ export default function Admin() {
 
     const createMarketWithGasless = async () => {
         let transactions = [];
-        const transactionData = predictionWorldInterface.encodeFunctionData("createMarket", [title, "", description, resolverUrl, timestamp]);
+        const transactionData = predictionWorldInterface.encodeFunctionData("createMarket", [title, imageUrl, description, resolverUrl, timestamp]);
 
         transactions = [
             {
@@ -74,104 +77,106 @@ export default function Admin() {
             }}
         >
             <AdminHeader />
-            <Container maxWidth="md" component="main">
-                <Link href="/admin/markets">
-                    <Button style={{ backgroundColor: "#1A84F2" }} variant="contained" fullWidth sx={{ mt: 2, mb: 2 }}>
-                        See All Markets
-                    </Button>
-                </Link>
-                <Box
-                    sx={{
-                        width: "100%",
-                        display: "flex",
-                        flexDirection: "column",
-                        mt: 1,
-                        borderColor: "divider",
-                        borderWidth: 1,
-                        borderRadius: 1,
-                        p: 2
-                    }}
-                >
-                    <Typography variant="h6" component="div" sx={{ mt: 1 }}>
-                        Add New Market
-                    </Typography>
-                    <Typography variant="subtitle1" component="div" sx={{ mt: 2, mb: 1 }}>
-                        Market Title
-                    </Typography>
-                    <TextField
-                        type="input"
-                        name="title"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        fullWidth
-                        placeholder="Title"
-                        autoComplete="off"
-                    />
-                    <Typography variant="subtitle1" component="div" sx={{ mt: 2, mb: 1 }}>
-                        Market Description
-                    </Typography>
-                    <TextField
-                        name="description"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        fullWidth
-                        placeholder="Description"
-                        autoComplete="off"
-                        multiline
-                    />
-                    <Typography variant="subtitle1" component="div" sx={{ mt: 2, mb: 1 }}>
-                        Market Image URL
-                    </Typography>
-                    <TextField
-                        type="input"
-                        name="resolverUrl"
-                        value={resolverUrl}
-                        onChange={(e) => setResolverUrl(e.target.value)}
-                        fullWidth
-                        placeholder="URL"
-                        autoComplete="off"
-                    />
-                    <Typography variant="subtitle1" component="div" sx={{ mt: 2, mb: 1 }}>
-                        Resolve URL
-                    </Typography>
-                    <TextField
-                        type="input"
-                        name="resolverUrl"
-                        value={resolverUrl}
-                        onChange={(e) => setResolverUrl(e.target.value)}
-                        fullWidth
-                        placeholder="URL"
-                        autoComplete="off"
-                    />
-                    <Typography variant="subtitle1" component="div" sx={{ mt: 2, mb: 1 }}>
-                        End Date
-                    </Typography>
-                    <TextField
-                        type="date"
-                        name="timestamp"
-                        // value={timestamp}
-                        onChange={(e) => {
-                            setTimestamp(e.target.valueAsDate?.getTime());
-                        }}
-                        fullWidth
-                        InputLabelProps={{
-                            shrink: true
-                        }}
-                    />
-                    <Button
-                        variant="contained"
-                        color="success"
-                        fullWidth
-                        style={{ backgroundColor: "#2e7d32" }}
-                        sx={{ mt: 2 }}
-                        onClick={() => {
-                            handleSubmit();
+            {!isPageLoading && (
+                <Container maxWidth="md" component="main">
+                    <Link href="/admin/markets">
+                        <Button style={{ backgroundColor: "#1A84F2" }} variant="contained" fullWidth sx={{ mt: 2, mb: 2 }}>
+                            See All Markets
+                        </Button>
+                    </Link>
+                    <Box
+                        sx={{
+                            width: "100%",
+                            display: "flex",
+                            flexDirection: "column",
+                            mt: 1,
+                            borderColor: "divider",
+                            borderWidth: 1,
+                            borderRadius: 1,
+                            p: 2
                         }}
                     >
-                        {submitButtonText}
-                    </Button>
-                </Box>
-            </Container>
+                        <Typography variant="h6" component="div" sx={{ mt: 1 }}>
+                            Add New Market
+                        </Typography>
+                        <Typography variant="subtitle1" component="div" sx={{ mt: 2, mb: 1 }}>
+                            Market Title
+                        </Typography>
+                        <TextField
+                            type="input"
+                            name="title"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            fullWidth
+                            placeholder="Title"
+                            autoComplete="off"
+                        />
+                        <Typography variant="subtitle1" component="div" sx={{ mt: 2, mb: 1 }}>
+                            Market Description
+                        </Typography>
+                        <TextField
+                            name="description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            fullWidth
+                            placeholder="Description"
+                            autoComplete="off"
+                            multiline
+                        />
+                        <Typography variant="subtitle1" component="div" sx={{ mt: 2, mb: 1 }}>
+                            Market Image URL
+                        </Typography>
+                        <TextField
+                            type="input"
+                            name="imageUrl"
+                            value={imageUrl}
+                            onChange={(e) => setImageUrl(e.target.value)}
+                            fullWidth
+                            placeholder="URL"
+                            autoComplete="off"
+                        />
+                        <Typography variant="subtitle1" component="div" sx={{ mt: 2, mb: 1 }}>
+                            Resolve URL
+                        </Typography>
+                        <TextField
+                            type="input"
+                            name="resolverUrl"
+                            value={resolverUrl}
+                            onChange={(e) => setResolverUrl(e.target.value)}
+                            fullWidth
+                            placeholder="URL"
+                            autoComplete="off"
+                        />
+                        <Typography variant="subtitle1" component="div" sx={{ mt: 2, mb: 1 }}>
+                            End Date
+                        </Typography>
+                        <TextField
+                            type="date"
+                            name="timestamp"
+                            // value={timestamp}
+                            onChange={(e) => {
+                                setTimestamp(e.target.valueAsDate?.getTime());
+                            }}
+                            fullWidth
+                            InputLabelProps={{
+                                shrink: true
+                            }}
+                        />
+                        <Button
+                            variant="contained"
+                            color="success"
+                            fullWidth
+                            style={{ backgroundColor: "#2e7d32" }}
+                            sx={{ mt: 2 }}
+                            onClick={() => {
+                                handleSubmit();
+                            }}
+                        >
+                            {submitButtonText}
+                        </Button>
+                    </Box>
+                </Container>
+            )}
         </Box>
     );
 }
