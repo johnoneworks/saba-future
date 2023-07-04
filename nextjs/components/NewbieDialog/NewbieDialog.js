97aadd1b3bf3214365a81e5d214dc4dddaa582f8
Lydia Huang <loveyleslie1013@gmail.com>
2023-07-04 16:36:44 +0800

    feat: add newbie dialog css

--- nextjs/components/NewbieDialog/NewbieDialog.js
+++ nextjs/components/NewbieDialog/NewbieDialog.js
@@ -1,12 +1,15 @@
 import { BiconomyAccountContext } from "@/contexts/BiconomyAccountContext";
 import { UserInfoContext } from "@/contexts/UserInfoContext";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
+import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import { useContext, useEffect, useState } from "react";
+import styles from "./NewbieDialog.module.scss";
 
 /**
  * TODO:
@@ -28,15 +31,34 @@ export const NewbieDialog = () => {
 
     return (
         <Dialog sx={{ textAlign: "center" }} onClose={() => setIsNewbie(false)} open={isNewbie}>
-            <DialogTitle sx={{ color: "#1A84F2" }}>Welcome to Saba Future</DialogTitle>
-            <CardContent>
-                <Typography sx={{ fontSize: 18, textAlign: "center" }} gutterBottom>
-                    You can get <span style={{ color: "#E84D4D" }}>1000 sure</span> after your first bet!
-                </Typography>
-            </CardContent>
-            <CardActions sx={{ display: "flex", justifyContent: "center" }}>
-                <Button onClick={() => setIsNewbie(false)}> Check it </Button>
-            </CardActions>
+            <Paper sx={{ backgroundImage: "linear-gradient(145deg, #1A84F2, #ce5eff)" }}>
+                <DialogTitle sx={{ fontSize: 16 }}>
+                    <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                        <span style={{ width: "10px", height: "1px", backgroundColor: "#fff", marginRight: "4px" }}></span>
+                        <span style={{ color: "#fff" }}>Welcome to SaBa Future</span>
+                        <span style={{ width: "10px", height: "1px", backgroundColor: "#fff", marginLeft: "4px" }}></span>
+                    </div>
+                </DialogTitle>
+                <Box
+                    sx={{
+                        color: "#fff",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center"
+                    }}
+                >
+                    <CardContent>
+                        <Typography sx={{ fontSize: 20 }} gutterBottom>
+                            Make your first bet and get <div style={{ fontWeight: "bold", fontSize: 30 }}>1000</div> free SURE tokens!
+                        </Typography>
+                    </CardContent>
+                    <CardActions sx={{}}>
+                        <Button onClick={() => setIsNewbie(false)} className={styles.btn}>
+                            Claim Now
+                        </Button>
+                    </CardActions>
+                </Box>
+            </Paper>
         </Dialog>
     );
 };
