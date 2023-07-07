a3037dfde760d11a73036815899652842d2abc00
Lydia Huang <loveyleslie1013@gmail.com>
2023-07-07 17:32:33 +0800

    feat: add statement empty page

---
+++ nextjs/components/EmptyPage/EmptyPage.js
@@ -0,0 +1,11 @@
+import ContentPasteOffIcon from "@mui/icons-material/ContentPasteOff";
+import { Box, Typography } from "@mui/material";
+
+export const EmptyPage = () => {
+    return (
+        <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%" }}>
+            <ContentPasteOffIcon sx={{ margin: 1, mt: 2, fontSize: 50, color: "#1A84F2" }} />
+            <Typography sx={{ color: "#1A84F2" }}>NO MARKET FOUND</Typography>
+        </Box>
+    );
+};
