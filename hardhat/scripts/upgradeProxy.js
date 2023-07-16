dbe8c85167ca8ee23797617bc60cfe5dc723f092
Phil Kau <elsevierphil@gmail.com>
2023-07-16 13:32:32 +0800

    feat: update the contract to add functions for editing the market

--- hardhat/scripts/upgradeProxy.js
+++ hardhat/scripts/upgradeProxy.js
@@ -2,18 +2,22 @@ const { ethers, upgrades } = require('hardhat');
 const { PROXY_ADDRESS, UPGRADING_CONTRACT } = process.env;
 
 async function main() {
-  const PredictionWorld = await ethers.getContractFactory(UPGRADING_CONTRACT);
-  const upgraded = await upgrades.upgradeProxy(PROXY_ADDRESS, PredictionWorld);
-  await upgraded.deployed();
+    const PredictionWorld = await ethers.getContractFactory(UPGRADING_CONTRACT);
+    const upgraded = await upgrades.upgradeProxy(
+        PROXY_ADDRESS,
+        PredictionWorld,
+        { timeout: 0 }
+    );
+    await upgraded.deployed();
 
-  const implementationAddress = await upgrades.erc1967.getImplementationAddress(
-    PROXY_ADDRESS
-  );
+    const implementationAddress = await upgrades.erc1967.getImplementationAddress(
+        PROXY_ADDRESS
+    );
 
-  console.log('                  Owner: ' + await upgraded.owner());
-  console.log('          Contract Name: ' + UPGRADING_CONTRACT);
-  console.log('         Proxy Contract: ' + PROXY_ADDRESS);
-  console.log('Implementation Contract: ' + implementationAddress);
+    console.log('                  Owner: ' + await upgraded.owner());
+    console.log('          Contract Name: ' + UPGRADING_CONTRACT);
+    console.log('         Proxy Contract: ' + PROXY_ADDRESS);
+    console.log('Implementation Contract: ' + implementationAddress);
 }
 
 main();
