7f96ca0d92d649aeb67d494a4d8cd3db0ef708fa
jim huang <jim.huang@onelab.tw>
2023-07-18 11:16:33 +0800

    feat: add GTM trace code

--- nextjs/pages/_document.js
+++ nextjs/pages/_document.js
@@ -1,13 +1,34 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head>
+                {/*GTM Tracker*/}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-5J6WX3X');`
+                    }}
+                />
+            </Head>
+            <body>
+                {/*GTM Tracker*/}
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-5J6WX3X"
+                        height="0"
+                        width="0"
+                        style={{ display: "none", visibility: "hidden" }}
+                    />
+                </noscript>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
