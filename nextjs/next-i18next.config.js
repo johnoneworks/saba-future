25df559920765c409322c450878314c03bb8e42b
sky.ren <rensky0742@gmail.com>
2023-09-21 18:10:41 +0800

    fix: config

--- nextjs/next-i18next.config.js
+++ nextjs/next-i18next.config.js
@@ -1,9 +1,7 @@
-import { LANGUAGES } from "./constants/Constant";
-
 module.export = {
     i18n: {
-        defaultLocale: LANGUAGES.EN,
-        locales: [LANGUAGES.EN, LANGUAGES.VN, LANGUAGES.IN, LANGUAGES.TH, LANGUAGES.ID],
+        defaultLocale: "en",
+        locales: ["en", "vn", "ind", "th", "id"],
         localePath: require("path").resolve("./public/locales")
     }
 };
